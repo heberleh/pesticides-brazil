d60628949fce6d79e9ab41b76a608d3a7a03b010
Henry Heberle <henryhbrl@gmail.com>
2019-05-07 17:31:01 -0300

    adding columns Pesticide, Concentration, VMP, percent VMP

---
+++ README.md
@@ -0,0 +1,8 @@
+# Pesticides Brazil dataset for Tableau
+
+This repository is dedicated to the configuration of the original Pesticides-Brazil dataset () for a visualization project using Tableau.
+
+# Modifications
+- Values "<LQ" were set to 0.0;
+- Values "<LD" are treated as 0.0;
+- Latitude/Longitude columns were removed;
--- setup_data_for_tableau.py
+++ setup_data_for_tableau.py
@@ -1,12 +1,18 @@
 import pandas as pd
 
+# extract the Pesticide Name from a column name
 def getPesticide(colName):
     return colName.split(" VMP: ")[0]
 
+
+# Extracts the VMP float value.
+# VMP stands for "Valor Máximo Permitido", in English: Maximum Value Allowed
 def getVMP(colName):
     name = colName.replace(" µg/L","")        
     return float(name.split(" VMP: ")[1])
 
+
+# column names
 def getPesticideColNames(csv):
     names = []
     for colName in csv.columns: 
@@ -14,11 +20,36 @@ def getPesticideColNames(csv):
             names.append(colName)
     return names
 
+def getNonPesticideColNames(csv):
+    names = []
+    for colName in csv.columns: 
+        if "VMP" not in colName:
+            names.append(colName)
+    return names
 
-csv = pd.read_csv("Vigilancia - Agrotóxicos 2014_2017.csv")
 
-# ... pesticide, concentration, percentage of VMP
 
-header = []
+# ... pesticide, concentration, VMP, percentage of VMP
+
+csv = pd.read_csv("pesticides_original.csv")
+
+pestCols = getPesticideColNames(csv)
+nonPestCols = getNonPesticideColNames(csv)
+
+header = nonPestCols + ["pesticide", "concentration", "VMP", "percentage of VMP"]
+matrix = []
 for index, row in csv.iterrows():
-    newRow = []
+    for pestCol in pestCols:        
+        if not pd.isna(row[pestCol]):
+            newRow = []
+            for col in nonPestCols:
+                newRow.append(row[col]) #add general attributes
+
+            newRow.append(getPesticide(pestCol)) #pesticide name
+            newRow.append(row[pestCol]) #concentration
+            newRow.append(getVMP(pestCol)) #max value permitted
+            newRow.append(float(row[pestCol])/getVMP(pestCol)) #percentage ox max value
+            
+            matrix.append(newRow) # store the new line
+        
+pd.DataFrame(matrix, columns=header).to_csv("pesticides_tableau.csv")
